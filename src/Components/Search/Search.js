a78b376c5105058ab0071ff2da52b0d1b102e56d
Adithya188288 <adithya.santhosh33@gmail.com>
2019-09-28 18:54:55 +0530

    made changes to search.js app.css cardlist.css search.css

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,36 +2,34 @@ import React from "react"
 // import DatePicker from "react-date-picker"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
-import { Button, Container } from "react-bootstrap"
+import { Button, Container, Row, Col } from "react-bootstrap"
 import "./Search.css"
 import { subDays, addDays } from "date-fns/esm"
 
 //Funtional Component - Does not contain state, only receive props
 const Search = props => {
   return (
-    <div className=" center">
-      <Container>
-        <DatePicker
-          onChange={props.onChangeDate}
-          selected={props.date}
-          dateFormat="yyyy/MM/dd"
-          minDate={subDays(props.date, props.getNoOfDaysFromToday(props.date))}
-          maxDate={addDays(new Date(), 0)}
-          showMonthDropdown
-          showYearDropdown
-          dropdownMode="select"
-          placeholderText="Select a date"
-        />
-        <Button
-          variant="success"
-          size="md"
-          className="custom-button-style"
-          onClick={props.onSearchUsers}
-        >
-          Get Users
-        </Button>
-      </Container>
-    </div>
+    <Container className="center">
+      <DatePicker
+        onChange={props.onChangeDate}
+        selected={props.date}
+        dateFormat="yyyy/MM/dd"
+        minDate={subDays(props.date, props.getNoOfDaysFromToday(props.date))}
+        maxDate={addDays(new Date(), 0)}
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+        placeholderText="Select a date"
+      />
+      <Button
+        variant="success"
+        size="md"
+        className="custom-button-style center"
+        onClick={props.onSearchUsers}
+      >
+        Get Users
+      </Button>
+    </Container>
   )
 }
 
